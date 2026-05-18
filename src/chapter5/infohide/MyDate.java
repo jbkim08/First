a20005f55007d868a8dd4e13d156c67a7120ec99
KJB <drv98@naver.com>
2026-05-18 11:27:51 +0900

    5장 정보은닉

---
+++ src/chapter5/infohide/MyDate.java
@@ -0,0 +1,39 @@
+package chapter5.infohide;
+
+public class MyDate {
+    private int day;
+    private int month;
+    private int year;
+
+    //변수에 접근할려면 get(가져옴) / set(입력) 메소드 사용
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        //2월일때 날짜를 28일까지만 적을수 있도록 제한
+        if(month == 2){
+            if(day < 1 || day >28){
+                System.out.println("오류입니다.");
+            } else {
+                this.day = day;
+            }
+        }
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
---
+++ src/chapter5/infohide/MyDateTest.java
@@ -0,0 +1,10 @@
+package chapter5.infohide;
+
+public class MyDateTest {
+    static void main() {
+        MyDate date = new MyDate();
+        date.setMonth(2);
+        date.setDay(31);
+        date.setYear(2018);
+    }
+}
