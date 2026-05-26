59b99315a8f860d1f867e3dee5d3ef48663c7ced
KJB <drv98@naver.com>
2026-05-26 10:22:13 +0900

    12장 컬렉션

--- src/chapter12/GenericMethod.java
+++ src/chapter12/GenericMethod.java
@@ -1,6 +1,7 @@
 package chapter12;
 
 public class GenericMethod {
+    //제네릭 메소드는 리턴타입 앞에 제네릭 타입
     public static <T,V> double makeRectangle(Point<T,V> p1, Point<T,V> p2){
         double left = ((Number)p1.getX()).doubleValue();
         double right = ((Number)p2.getX()).doubleValue();
@@ -14,10 +15,11 @@ public class GenericMethod {
     }
 
     static void main() {
+        //기본타입 int, double, boolean => 래퍼클래스 Integer, Double, Boolean
         Point<Integer, Double> p1 = new Point<>(0, 0.0);
         Point<Integer, Double> p2 = new Point<>(10, 10.0);
 
-        double rect = GenericMethod.makeRectangle(p1,p2);
+        double rect = GenericMethod.<Integer, Double>makeRectangle(p1,p2);
         System.out.println("두 점으로 만들어진 사각형의 넓이는 " + rect + "입니다.");
     }
 }
