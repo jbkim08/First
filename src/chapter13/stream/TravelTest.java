cd426ff6726097fdf01f7e0e62eff06374033c97
KJB <drv98@naver.com>
2026-05-27 12:25:51 +0900

    13장 스트림4

--- src/chapter13/stream/TravelTest.java
+++ src/chapter13/stream/TravelTest.java
@@ -38,5 +38,26 @@ public class TravelTest {
         TravelCustomer customerHong = new TravelCustomer("홍길동", 13, 50);
         List<TravelCustomer> customerList = new ArrayList<>();
         customerList.add(customerLee);
+        customerList.add(customerKim);
+        customerList.add(customerHong);
+        System.out.println("=== 고객 명단 추가된 순서대로 출력 ===");
+        customerList.stream()
+                .map(c->c.getName())
+                .forEach(s-> System.out.println(s));
+        int total = customerList.stream()
+                .mapToInt(c->c.getPrice())
+                .sum();
+        int total2 = customerList.stream()
+                .map(c->c.getPrice())
+                .reduce(0, (n1,n2)-> n1+n2);
+        System.out.println("총 여행 경비는 : " + total + "만 원입니다.");
+        System.out.println("총 여행 경비는 : " + total2 + "만 원입니다.");
+
+        System.out.println("=== 20세 이상 고객 명단 정렬해서 출력 ==");
+        customerList.stream()
+                .filter(c->c.getAge() >= 20)
+                .map(c->c.getName())
+                .sorted()
+                .forEach(s-> System.out.println(s));
     }
 }
